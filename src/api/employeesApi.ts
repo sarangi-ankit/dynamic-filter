bcbebc3a2c10377bf2cf3f0eaf5dcf9ac4d0710f
Ankit Sarangi <sarangiankit097@gmail.com>
2026-01-13 10:56:49 +0530

    update db

--- src/api/employeesApi.ts
+++ src/api/employeesApi.ts
@@ -1,9 +1,12 @@
+// src/api/employeesApi.ts
 import type { Employee } from "../types/data";
 
-const BASE_URL = "http://localhost:4000/employees";
+const BASE_URL = "/db.json";
 
 export async function fetchEmployees(): Promise<Employee[]> {
   const res = await fetch(BASE_URL);
   if (!res.ok) throw new Error("Failed to fetch employees");
-  return res.json();
+
+  const data = await res.json();
+  return data.employees; // 👈 extract array
 }
